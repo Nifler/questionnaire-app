cf1235477761919d30baa5f1605d411e0dd6f588
Nifler <nifler11@gmail.com>
2023-06-30 11:56:56 +0300

    Services

---
+++ app/Services/Poll/ConditionService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Poll;
+
+use App\Models\Question;
+use App\Models\User;
+
+class ConditionService
+{
+    public function isAllowedQuestion(User $user, Question $question)
+    {
+
+    }
+}
---
+++ app/Services/Poll/PollServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\Poll;
+
+use Illuminate\Support\ServiceProvider;
+
+class PollServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
