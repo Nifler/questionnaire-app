036fb0f9a1a4c1c5af9f077b0b4a45b7d1bc363e
Nifler <nifler11@gmail.com>
2026-03-10 20:18:05 +0200

    ASANA-1213590244658083 fix auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,6 @@ class Kernel extends HttpKernel
             AdminGate::class,
         ],
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
